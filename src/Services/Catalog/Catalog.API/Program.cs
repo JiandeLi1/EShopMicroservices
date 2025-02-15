32a79649e130c9ead7857511a7f27508e6a5a2f0
Jiande Li <jli@stellarservices.com>
2025-02-14 22:18:02 -0500

    Adding custom Exceptions handler

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -4,25 +4,29 @@ using BuildingBlocks.Behaviors;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
 //Add services to the container
-builder.Services.AddCarter();
+
 builder.Services.AddMediatR(config => 
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
 
-builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+builder.Services.AddValidatorsFromAssembly(assembly);
 
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
-
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
 
 //Configure the HTTP request pipeline
-app.MapCarter(); 
+app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
 app.Run();
