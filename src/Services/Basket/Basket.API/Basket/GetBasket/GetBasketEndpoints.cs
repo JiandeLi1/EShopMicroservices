915f73fa1f67da849f0a88a0b66034f952fb7d51
Jiande Li <jli@stellarservices.com>
2025-02-18 21:56:31 -0500

    Create mock get shopping cart method

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API.Models;
+
+namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketResponse(ShoppingCart Cart);
+public class GetBasketEndpoints : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        throw new NotImplementedException();
+    }
+}
